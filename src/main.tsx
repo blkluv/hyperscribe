082d1ae37b565c61e71ac039f416c55d68dc76a2
Desk888 <work.lorenzofilippinI@gmail.com>
2025-04-04 11:08:03 +0100

    Installed Google Tag Manager

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import TagManager from "react-gtm-module"
+
+const tagManagerArgs = {
+    gtmId: "GTM-TVZCBR63"
+}
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById("root")!).render(<App />);
